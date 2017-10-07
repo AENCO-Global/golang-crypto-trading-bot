a40a306e5b25bf9e0755163c6b63e3ed03abefa3
Alex <Alex@Alexs-MBP.fritz.box>
2017-10-07 10:48:02 +0200

    Initial Yobit wrapper

--- exchangeWrappers/generic_exchange_wrapper.go
+++ exchangeWrappers/generic_exchange_wrapper.go
@@ -1,6 +1,6 @@
 package exchangeWrappers
 
-import "github.com/saniales/golang-crypto-trading-bot/environment"
+import "github.com/Alexbrem/golang-crypto-trading-bot/environment"
 
 //ExchangeWrapper provides a generic wrapper for exchange services.
 type ExchangeWrapper interface {
--- exchangeWrappers/yobit.go
+++ exchangeWrappers/yobit.go
@@ -1,1 +1,18 @@
 package exchangeWrappers
+import (
+	"github.com/Alexbrem/golang-crypto-trading-bot/environment"
+
+	yobitAPI "github.com/Alexbrem/golang-crypto-trading-bot/exchangeWrappers/yobitapiwrap"
+)
+
+// YobitWrapper provides a Generic wrapper of the Yobit API.
+type YobitWrapper struct {
+	yobitAPI *yobitAPI.Yobit //Represents the helper of the Yobit API.
+}
+
+// NewYobitWrapper creates a generic wrapper of the Yobit API.
+func NewYobitWrapper(apiKey string, apiSecret string) ExchangeWrapper {
+	return YobitWrapper{
+		yobitAPI: yobitAPI.New(apiKey, apiSecret),
+	}
+}
